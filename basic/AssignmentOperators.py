62c34561dbe421a83ca052202f2d367d730737a9
Riya Kumari <riyaraju8412@gmail.com>
2026-03-25 10:05:44 +0530

    Add Assignment and Logical Operators program

---
+++ basic/AssignmentOperators.py
@@ -0,0 +1,14 @@
+a = 43 # a is assigned with 43
+a += 3 
+print(a)
+a -= 2
+print(a)
+a *= 1
+print(a)
+a /= 2
+print(a)
+a %= 5
+print(a)
+
+a **= 3
+print(a)
---
+++ basic/LogicalOperators.py
@@ -0,0 +1,14 @@
+# not => return opposite 
+flag = True
+print(not flag) # False 
+print(not (4>5)) # True
+
+# and => both value true
+
+a = 5
+b = 2
+print((a>b) and (a>=b)) # True
+
+# or => return if anyone is true
+
+print((a>b) or (a<b)) # True
