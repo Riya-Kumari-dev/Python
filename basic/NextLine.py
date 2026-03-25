5e871499222b2348482f007a10f8fad8b417b60e
Riya Kumari <riyaraju8412@gmail.com>
2026-03-25 09:10:00 +0530

    Initial Python programs

---
+++ basic/NextLine.py
@@ -0,0 +1,6 @@
+# If we want next line then we have to use different print statements
+print("Riya Gupta")
+print("A Passionate student.")
+
+# Or if we want it to be on the same line we use , and , also gives a space
+print("Riya Gupta","is a passionate student.")
