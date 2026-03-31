b45917b500000110bd5e3a4d8652ccc9f6518da3
Riya Kumari <riyaraju8412@gmail.com>
2026-03-31 18:56:35 +0530

    Add membership operator program

---
+++ src/basic/MembershipOperator.py
@@ -0,0 +1,9 @@
+a ="Riya Gupta"
+print('i' in a) # True
+print("iya" in a) # True
+
+l1 = [45,"Riy", 43.2] 
+print(43 in l1) # False
+print("Riy" in l1) # True
+print('i' in l1) # False
+print(45 not in l1) # False
