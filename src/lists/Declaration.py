0bd68677b45042942a8fc5e66759407532ffbc9a
Riya Kumari <riyaraju8412@gmail.com>
2026-04-04 19:37:21 +0530

    Add list declaration program

---
+++ src/lists/Declaration.py
@@ -0,0 +1,20 @@
+# lists
+# - Duplicate allowed 
+# - Order preserve
+# - Slicing
+# - Indexing
+# - Mutable 
+# - Heterogeneous
+
+l = [4,3,"Riya",5] 
+print(l)
+l2 = [] 
+for i in range(1,6) :
+    l2.append(i)
+print(l2)
+
+l3 = list(range(1,6))
+print(l3)
+l4 = eval(input("Enter a list : "))
+print(type(l4))
+print(l4)
