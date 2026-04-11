1d505a002853e350fdf9765859c30a37854f2f9f
Riya Kumari <riyaraju8412@gmail.com>
2026-04-11 09:25:22 +0530

    Modify ReturnMultipleValues.py file

--- src/functions/ReturnMultipleValues.py
+++ src/functions/ReturnMultipleValues.py
@@ -6,4 +6,4 @@ sum,diff = f(10,20)
 print(sum) # 30
 print(diff) # -10
 print(type(f(1,2))) # tuple
-print(f.__doc__) 
+print(f.__doc__)  # object.__doc__ => return doc string => This function returns sum and difference of two parameters
